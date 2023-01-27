0eb838c70e0b46699f7257b36c499369f5a268ed
andrzej <a.pruszon@gmail.com>
2023-01-27 10:29:44 +0100

    flickr-gallery - add comments

--- src/components/common/apiError.tsx
+++ src/components/common/apiError.tsx
@@ -1,6 +1,9 @@
 interface IApiErrorProps {
   error: any;
 }
+// TODO:
+// I could do better implementation od error message maping response from API.
+// To do it i would use 2 sets of enums
 
 const ApiError: React.FC<IApiErrorProps> = ({ error }) => {
   return <div>{JSON.stringify(error)}</div>;
--- src/components/common/loading.tsx
+++ src/components/common/loading.tsx
@@ -1,3 +1,6 @@
+// TODO:
+// Here should be a spinner
+
 const Loading = () => {
   return <p>Loading...</p>;
 };
